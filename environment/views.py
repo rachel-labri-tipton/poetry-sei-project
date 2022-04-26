90967144c3454e4805d06e159061fb19a3bf9d5d
Rachel Tipton <RACHELMTIPTON@GMAIL.COM>
2022-04-26 23:24:55 +0200

    finalizing deployment

--- environment/views.py
+++ environment/views.py
@@ -3,8 +3,6 @@ from rest_framework import generics
 from environment.models import Environment
 from environment.serializers import EnvironmentSerializer
 
-# Create your views here. And there is the controller-like logic in the views
-
 
 class EnvironmentListView(generics.ListCreateAPIView):
     queryset = Environment.objects.all()
